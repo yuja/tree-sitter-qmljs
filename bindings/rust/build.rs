c6cb4650cea504e71781702a1494a16b75530ab2
Antonin Delpeuch <antonin@delpeuch.eu>
2025-10-09 08:55:34 +0200

    Update Rust bindings

--- bindings/rust/build.rs
+++ bindings/rust/build.rs
@@ -2,11 +2,7 @@ fn main() {
     let src_dir = std::path::Path::new("src");
 
     let mut c_config = cc::Build::new();
-    c_config.include(&src_dir);
-    c_config
-        .flag_if_supported("-Wno-unused-parameter")
-        .flag_if_supported("-Wno-unused-but-set-variable")
-        .flag_if_supported("-Wno-trigraphs");
+    c_config.std("c11").include(src_dir);
 
     #[cfg(target_env = "msvc")]
     c_config.flag("-utf-8");
@@ -16,12 +12,10 @@ fn main() {
     println!("cargo:rerun-if-changed={}", parser_path.to_str().unwrap());
 
     let scanner_path = src_dir.join("scanner.c");
-    c_config.file(&scanner_path);
-    println!("cargo:rerun-if-changed={}", scanner_path.to_str().unwrap());
-    println!(
-        "cargo:rerun-if-changed={}",
-        src_dir.join("typescript-scanner.h").to_str().unwrap()
-    );
+    if scanner_path.exists() {
+        c_config.file(&scanner_path);
+        println!("cargo:rerun-if-changed={}", scanner_path.to_str().unwrap());
+    }
 
-    c_config.compile("parser-scanner");
+    c_config.compile("tree-sitter-qmljs");
 }
