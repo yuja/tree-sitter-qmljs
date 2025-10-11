104ce50a2a7c2fbc6e580d2e4d9a000cd8d306dc
Yuya Nishihara <yuya@tcha.org>
2025-10-11 21:13:54 +0900

    regenerate Rust bindings

--- bindings/rust/build.rs
+++ bindings/rust/build.rs
@@ -2,11 +2,7 @@ fn main() {
     let src_dir = std::path::Path::new("src");
 
     let mut c_config = cc::Build::new();
-    c_config.include(&src_dir);
-    c_config
-        .flag_if_supported("-Wno-unused-parameter")
-        .flag_if_supported("-Wno-unused-but-set-variable")
-        .flag_if_supported("-Wno-trigraphs");
+    c_config.std("c11").include(src_dir);
 
     #[cfg(target_env = "msvc")]
     c_config.flag("-utf-8");
@@ -23,5 +19,5 @@ fn main() {
         src_dir.join("typescript-scanner.h").to_str().unwrap()
     );
 
-    c_config.compile("parser-scanner");
+    c_config.compile("tree-sitter-qmljs");
 }
